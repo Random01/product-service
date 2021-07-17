92b51b850be67d093a4d769a691c1bb4f1605ee8
Alexsei.Matvienko <Alexsei.Matvienko@thomsonreuters.com>
2021-07-17 07:50:10 +0400

    - add aws x-ray logging

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { ProductsRouter } from './src/products';
 
+import AWSXRay from 'aws-xray-sdk';
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 const appUuid = uuidv4();
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true,
 }));
+app.use(AWSXRay.express.openSegment('ProductService'));
 
 app.get('/', (_, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -22,6 +25,8 @@ app.get('/state', (_, res) => {
 });
 
 new ProductsRouter(app);
+
+app.use(AWSXRay.express.closeSegment());
 
 app.listen(PORT, () => {
   console.log(`Product Service App is listening on port ${PORT}!`);
